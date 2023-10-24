383d2cddc0c924f44bcf4ff36e64a4db0a9899b3
hector <hector2024.hectorcordova2024@gmail.com>
2023-10-24 23:09:45 +0000

    Moved the script to under main

--- helloworld.sh
+++ helloworld.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "Hello world!"
+
 # Script Name:                  Hello World!
 # Author:                       Hector Cordova
 # Date of latest revision:      10/24/2023
 # Purpose:                      Greetings
 
 # Declaration of variables
-
+user: how are you doing? 
 # Declaration of functions
 
 
 # Main
-
+echo "Hello world!"
 
 
 # End
