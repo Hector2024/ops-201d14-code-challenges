662d28e9dac03a0e96ce2762c43c38578a24e58b
hector <hector2024.hectorcordova2024@gmail.com>
2023-10-24 22:51:58 +0000

    Challenge 1 for ops201

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Hello World!
+# Author:                       Hector Cordova
+# Date of latest revision:      10/24/2023
+# Purpose:                      Greetings
+
+# Declaration of variables
+none
+
+# Declaration of functions
+to say hi
+
+# Main
+
+
+
+# End
