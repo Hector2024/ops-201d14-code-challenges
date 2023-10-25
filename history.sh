6f7cc36853e71b3a925a48348782f4793de004eb
Hector Cordova <hectorcordova2024@gmail.com>
2023-10-25 19:53:54 -0400

    Created file,  added gen info (name, author,date,and purpose)

---
+++ history.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  history.sh
+# Author:                       Hector Cordova
+# Date of latest revision:      10/25/2023
+# Purpose:                      Show login History
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
