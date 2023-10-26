cd06e61e299a416a10bc1f82587e7d8043e26621
Hector Cordova <hectorcordova2024@gmail.com>
2023-10-26 13:29:20 -0400

    testing out declartion of variables

---
+++ passingBy.sh
@@ -0,0 +1,19 @@
+# Declaration of variables
+
+message="WHO GOES THERE!"
+message2="GO AWAY"
+
+# Declaration of functions
+
+passingBy() {
+    echo "$message"
+ 
+    echo "$message2"
+ 
+}
+
+# Main
+
+passingBy
+
+# End
