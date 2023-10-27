d7740484de39049c2fddef57e4a8298d83281845
Hector Cordova <hectorcordova2024@gmail.com>
2023-10-27 18:32:14 -0400

    forgot to save, last still holds true

---
+++ rubegoldberg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script Name:                  rubegoldberg.sh
+# Author:                       Hector Cordova
+# Date of latest revision:      10/27/2023
+# Purpose:                      ask for PID, accept PID, die
+# Exexcute command:             bash ./rubegoldberg.sh
+
+# Declaration of variables
+
+# Declaration of functions
+
+PID() {
+    
+
+ 
+}
+
+# Main
+
+
+
+# End
