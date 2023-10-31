de489eae8ddf3ff5e475ac29407b7bbedcb0c83e
Hector Cordova <hectorcordova2024@gmail.com>
2023-10-31 13:02:42 -0400

    copied demo still need to edit

--- rubegoldberg.sh
+++ rubegoldberg.sh
@@ -12,10 +12,29 @@
 
 
  
-if [ $var -ne 123 ]
-then 
 
-echo $var 
+# Basic for loop
+names="Stan Kyle Cartman"
+for name in $names
+do
+  echo $name
+done
+
+echo -e "For Loop complete\n"
+
+
+# Basic while loop
+
+while true
+do
+  echo -e "What would you like me to say?\n"
+  read input
+  echo $input
+
+  echo -e "\nTo stop this script at any time, press Ctrl + C\n"
+done
+
+
 
 # Main
 
